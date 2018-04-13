77cde023b3dbd1370e628f1b7ca0705605c00bba
Won Kyoung Park <wpark@hmc.edu>
2018-04-13 13:52:36 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :stores
   get 'welcome/index'
 
-  resources :stores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
